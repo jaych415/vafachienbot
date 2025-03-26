707ee042937a7f6242d4615225f00ef23eb64445
Jayesh Chaudhary <jayeshchaudhary@Jayeshs-MacBook-Pro-2.local>
2025-03-26 12:42:11 -0700

    Change command prefix to '-'

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 # Bot setup
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
